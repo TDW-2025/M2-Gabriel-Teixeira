abff6a02507b92af4c9edb9e60d8b9135a343204
Gabriel Teixeira <gabrielm.teixeira@ua.pt>
2025-11-03 11:38:21 +0000

    index page para a sepação dos projetos done

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,7 @@
 import React, { useState, useEffect } from "react";
 import PageTodolist from "./pages/PageTodolist.jsx";
 import PageTodolist2 from "./pages/PageTodolist2.jsx";
-
-
+import Index from "./pages/Index.jsx";
 
 
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
@@ -11,13 +10,11 @@ import "./App.css";
 
 function App() {
 
-  // adicionar aqui o react router, depois entre cada pagia usar o Link do react router 
-
   return (
 
      <Router> 
       <Routes>
-        <Route path="/*" />
+        <Route path="/" element={<Index />}/>
         <Route path="/pageTodolist" element={<PageTodolist />} />
         <Route path="/pageTodolist2" element={<PageTodolist2 />} />
       </Routes>
